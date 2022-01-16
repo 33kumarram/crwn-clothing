a6931f6b1521247395d5ae300e572a54691e75ec
33kumarram <95571520+33kumarram@users.noreply.github.com>
2022-01-16 17:13:58 +0530

    updating our app with reselect, replaced cart selectors with memized selector

--- src/components/cartdropdown/cartdropdown.jsx
+++ src/components/cartdropdown/cartdropdown.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { selectCartItems } from '../../redux/cart/cart.selector';
 import CartItemd from '../cartitem/cartitem.component';
 import CustomButton from '../custom button/custombutton';
 import './cartdropdown.style.scss';
@@ -16,7 +17,10 @@ const CartDropdown=({ cartItems }) =>(
         </div>
     
 )
-const mapStateToProps =({cart :{cartItems}}) =>({
-    cartItems
+const mapStateToProps =(state) =>({
+    cartItems : selectCartItems(state)
 })
+// const mapStateToProps =({cart :{cartItems}}) =>({
+//     cartItems
+// })
 export default connect(mapStateToProps)(CartDropdown);
--- src/components/carticon/carticon.component.jsx
+++ src/components/carticon/carticon.component.jsx
@@ -1,18 +1,25 @@
 import React from "react";
 import { connect } from "react-redux";
+import { selectCartItemsCount } from "../../redux/cart/cart.selector";
 import {toggleCartHidden}  from "../../redux/cart/action";
 import {ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 import './carticon.style.scss'
-const CartIcon = ({toggleCartHidden}) =>{
+const CartIcon = ({toggleCartHidden, itemCount}) =>{
    return( 
    <div className="cart-icon" onClick= {toggleCartHidden} >
          <ShoppingIcon className="shopping-icon" />,
-        <span className="item-count"> 0</span>
+        <span className="item-count"> {itemCount}</span>
     </div>
    )
 }
 const mapDispatchToProps = dispatch =>({
    toggleCartHidden :()=>dispatch(toggleCartHidden())
 })
+const mapStateToPros = (state) =>({
+    itemCount: selectCartItemsCount(state)
+})
+// const mapStateToPros = ({cart :{cartItems}}) =>({
+//    itemCount : cartItems.reduce((accumulatedquantity, cartItem)=> accumulatedquantity + cartItem.quantity , 0)
+// })
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+export default connect(mapStateToPros, mapDispatchToProps)(CartIcon);
