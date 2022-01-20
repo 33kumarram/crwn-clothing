30d178caa097f7440f88cc7eaa450033d05eb8e5
33kumarram <95571520+33kumarram@users.noreply.github.com>
2022-01-20 07:37:55 +0530

    added checkout page and also used selector

--- src/components/cartdropdown/cartdropdown.jsx
+++ src/components/cartdropdown/cartdropdown.jsx
@@ -1,22 +1,38 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom'
 import { connect } from 'react-redux';
 import { selectCartItems } from '../../redux/cart/cart.selector';
+import {toggleCartHidden} from '../../redux/cart/action'
 import CartItemd from '../cartitem/cartitem.component';
 import CustomButton from '../custom button/custombutton';
 import './cartdropdown.style.scss';
 
-const CartDropdown=({ cartItems }) =>(
+
+const CartDropdown=({ cartItems, dispatch}) =>{
+    const navigate =useNavigate();
+    
+    return(
 
         <div className='cart-dropdown' >
             <div className="cart-items">
                 {
+                cartItems.length 
+                ?
                 cartItems.map (cartItem=> <CartItemd key={cartItem.id} item={cartItem}/>)
+                :
+                <span className="empty-message"> Your cart is empty </span>
                 }
+
             </div>
-                <CustomButton>GO TO CHECKOUT</CustomButton>
+                <CustomButton onClick={
+                    ()=>{navigate('/checkout');
+                    dispatch(toggleCartHidden())
+                    }} >
+                    GO TO CHECKOUT
+                    </CustomButton>
         </div>
     
-)
+)}
 const mapStateToProps =(state) =>({
     cartItems : selectCartItems(state)
 })
--- src/components/carticon/carticon.component.jsx
+++ src/components/carticon/carticon.component.jsx
@@ -3,6 +3,7 @@ import { connect } from "react-redux";
 import { selectCartItemsCount } from "../../redux/cart/cart.selector";
 import {toggleCartHidden}  from "../../redux/cart/action";
 import {ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
+import { createStructuredSelector } from "reselect";
 import './carticon.style.scss'
 const CartIcon = ({toggleCartHidden, itemCount}) =>{
    return( 
@@ -15,8 +16,8 @@ const CartIcon = ({toggleCartHidden, itemCount}) =>{
 const mapDispatchToProps = dispatch =>({
    toggleCartHidden :()=>dispatch(toggleCartHidden())
 })
-const mapStateToPros = (state) =>({
-    itemCount: selectCartItemsCount(state)
+const mapStateToPros = createStructuredSelector({
+    itemCount: selectCartItemsCount
 })
 // const mapStateToPros = ({cart :{cartItems}}) =>({
 //    itemCount : cartItems.reduce((accumulatedquantity, cartItem)=> accumulatedquantity + cartItem.quantity , 0)
