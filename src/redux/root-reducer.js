3b39258f52d304793cd2a7e4f370c6223fafd775
33kumarram <95571520+33kumarram@users.noreply.github.com>
2022-02-17 07:34:51 +0530

    changed state item with reducer

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,8 +1,22 @@
 import { combineReducers } from "redux";
+import persistReducer from "redux-persist/es/persistReducer";
+import storage from "redux-persist/lib/storage";
+
+
 import userReducer from "./user/user-reducer";
 import cartReducer from "./cart/cartreducer";
+import shopReducer from "./shop/shopreducer";
 
-export default combineReducers ({
+const persistConfig = {
+    key:'root',
+    storage,
+    whitelist: ['cart']
+};
+const rootReducer= combineReducers ({
     user: userReducer,
-    cart : cartReducer
+    cart: cartReducer,
+    shop: shopReducer
 });
+
+
+export default persistReducer(persistConfig,rootReducer);
